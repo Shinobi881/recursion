f58cb571ce55ba274816da1463b57f657bf8d2d5
Moose <moose.techguy@gmail.com>
2015-06-26 19:05:23 -0700

    Working on objects now

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,19 +5,53 @@
 
 var stringifyJSON = function(obj) {
 	// Unstringifiables 
-	if (typeof obj == 'function') {
+	if (typeof obj === 'function' || typeof obj === 'undefined') {
 		return '';
-	}
-
-	if (typeof obj == 'undefined') { 
-		return'';
 	}
 	
 	// Stringifiables' conditionals
-	if (obj === null || typeof obj === 'number' || typeof obj === 'boolean')
-	return '' + obj + '';
+	if(obj === null){
+		return obj + '';
+	} 
+
+	if (typeof obj === 'number' || typeof obj === 'boolean')
+	return obj.toString();
 
 	if (typeof obj === 'string') 
-		return '"'+obj+'"';
-}
+		return '"' + obj + '"';
+
+
+	var isEmptyObject = obj ? [] : {};
+
+	console.log(isEmptyObject);
+
+	var result;
+
+	// Object testing
+	if (obj.constructor === Array) {
+	  		if (isEmptyObject) {
+	  			return 'obj';
+	  		} else {
+	  			result = '['
+	  			_.each(obj, function(element){result += stringifyJSON(element) + ','});
+	  		console.log('Result for array', result); 
+	  		return result += ']';
+	  		}
+	  	}
+// -----------------------------------------------
+
+	if(obj.constructor === Object){
+		if(isEmptyObject){
+			return '{}'
+		} else {
+			result = '{';
+			_.each(obj, function(value, key){ result += '\'' + key + '\':' + stringifyJSON(value) + ','});
+			console.log('Result for array', result);
+		return result += '}';
+		}	
+	}
+	
+
 };
+
+	
